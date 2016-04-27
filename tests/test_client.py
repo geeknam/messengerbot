36a4bedfb51a582960c18de96343bba3eabfc14e
Juan Madurga <jlmadurga@gmail.com>
2016-04-27 17:42:22 +0200

    fix error handling when no error_data

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from messengerbot import MessengerClient, messages
+from messengerbot import MessengerClient, messages, MessengerException
 from mock import patch
 
 
@@ -20,6 +20,58 @@ class ClientTestCase(TestCase):
         request = messages.MessageRequest(recipient, message)
         messenger.send(request)
 
+        mock_post.assert_called_with(
+            'https://graph.facebook.com/v2.6/me/messages',
+            json={
+                "message": {"text": "Hello World"},
+                "recipient": {"id": "123"}
+            },
+            params={'access_token': '1234'}
+        )
+        
+    @patch('requests.post')
+    def test_client_send_error_with_error_data(self, mock_post):
+
+        mock_post.return_value.status_code = 190
+        mock_post.return_value.json.return_value = {
+            "error":{
+                     "message":"Invalid parameter",
+                     "type":"FacebookApiException",
+                     "code":100,
+                     "error_data":"No matching user found.",
+                     "fbtrace_id":"D2kxCybrKVw"
+                     }
+        }
+
+        messenger = MessengerClient(access_token='1234')
+
+        recipient = messages.Recipient(recipient_id='123')
+        message = messages.Message(text='Hello World')
+        request = messages.MessageRequest(recipient, message)
+        self.assertRaises(MessengerException, messenger.send, request)
+        mock_post.assert_called_with(
+            'https://graph.facebook.com/v2.6/me/messages',
+            json={
+                "message": {"text": "Hello World"},
+                "recipient": {"id": "123"}
+            },
+            params={'access_token': '1234'}
+        )
+        
+    @patch('requests.post')
+    def test_client_send_error_with_no_error_data(self, mock_post):
+
+        mock_post.return_value.status_code = 190
+        mock_post.return_value.json.return_value = {
+            'error': {u'message': u'This Page access token belongs to a Page that has been deleted.', u'code': 190, u'type': u'OAuthException'}
+        }
+
+        messenger = MessengerClient(access_token='1234')
+
+        recipient = messages.Recipient(recipient_id='123')
+        message = messages.Message(text='Hello World')
+        request = messages.MessageRequest(recipient, message)
+        self.assertRaises(MessengerException, messenger.send, request)
         mock_post.assert_called_with(
             'https://graph.facebook.com/v2.6/me/messages',
             json={
